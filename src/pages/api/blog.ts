2a9a5b387d542bf0e9d06d1dd603509613715646
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-07-28 22:26:36 -0700

    original

--- src/pages/api/blog.ts
+++ src/pages/api/blog.ts
@@ -1,48 +1,38 @@
-import { NextRequest, NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from 'next'
 const { Configuration, OpenAIApi } = require("openai");
 
-export const config = {
-  runtime: 'edge',
+type Data = {
+  name: string
 }
 
-export default async function handler(req: any) {
-  const json = await req.json()
-  const openAIKey = json['openAIKey']
-  const keywords = json['keywords']
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const openAIKey = JSON.parse(req.body)['openAIKey']
+  const keywords = JSON.parse(req.body)['keywords']
 
-  try {
-    const configuration = new Configuration({
-      apiKey: openAIKey
-    });
 
-    const openai = new OpenAIApi(configuration);
+  const configuration = new Configuration({
+    apiKey: openAIKey
+  });
 
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      stream: false,
-      messages: [
-        {
-          role: 'user',
-          content: `Write an extremely SEO optimized blog post about the following keywords: ${keywords}. Make sure to include the keywords in the title, headings, and content. Make sure the article is no more than 1500 words. The output has to be in MARKDOWN.`
-        }
-      ],
-      max_tokens: 3500,
-      temperature: 0.9,
-    })
+  const openai = new OpenAIApi(configuration);
 
-    console.log(response.data.choices[0].message.content)
-
-    return new NextResponse(response.data.choices[0].message.content
-      , {
-        status: 200,
-        headers: {
-          'content-type': 'application/json',
-        },
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: false,
+    // a precise prompt is important for the AI to reply with the correct tokens
+    messages: [
+      {
+        role: 'user',
+        content: `Write an extremely SEO optimized blog post about the following keywords: ${keywords}. Make sure to include the keywords in the title, headings, and content. Make sure the article is no more than 400 words. The output has to be in MARKDOWN.`
       }
-    );
-  } catch (e) {
-    console.log(e)
-    //@ts-ignore
-    return Response.json()
+    ],
+    max_tokens: 3500,
+    temperature: 0.9, // you want absolute certainty for spell check
+  })
+
+  if (response.status != 200) {
+    res.status(500).json({ name: 'Error' })
   }
+
+  res.status(200).json(response.data.choices[0].message.content)
 }
