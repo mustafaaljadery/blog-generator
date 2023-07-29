0325e4b980ff5914ac6e51df5739b893c8194e25
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-07-28 22:18:41 -0700

    test

--- src/pages/api/blog.ts
+++ src/pages/api/blog.ts
@@ -1,41 +1,39 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest, NextResponse } from 'next/server';
 const { Configuration, OpenAIApi } = require("openai");
-
-type Data = {
-  name: string
-}
 
 export const config = {
   runtime: 'edge',
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const openAIKey = JSON.parse(req.body)['openAIKey']
-  const keywords = JSON.parse(req.body)['keywords']
+export default async function handler(req: NextRequest, res: any) {
+  const json = await req.json()
+  const openAIKey = json['openAIKey']
+  const keywords = json['keywords']
 
+  try {
+    const configuration = new Configuration({
+      apiKey: openAIKey
+    });
 
-  const configuration = new Configuration({
-    apiKey: openAIKey
-  });
+    const openai = new OpenAIApi(configuration);
 
-  const openai = new OpenAIApi(configuration);
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stream: false,
+      messages: [
+        {
+          role: 'user',
+          content: `Write an extremely SEO optimized blog post about the following keywords: ${keywords}. Make sure to include the keywords in the title, headings, and content. Make sure the article is no more than 1500 words. The output has to be in MARKDOWN.`
+        }
+      ],
+      max_tokens: 3500,
+      temperature: 0.9,
+    })
 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: false,
-    messages: [
-      {
-        role: 'user',
-        content: `Write an extremely SEO optimized blog post about the following keywords: ${keywords}. Make sure to include the keywords in the title, headings, and content. Make sure the article is no more than 1500 words. The output has to be in MARKDOWN.`
-      }
-    ],
-    max_tokens: 3500,
-    temperature: 0.9,
-  })
-
-  if (response.status != 200) {
-    res.status(500).json({ name: 'Error' })
+    res.status(200).json(response.data.choices[0].message.content);
+  } catch (e) {
+    console.log(e)
+    //@ts-ignore
+    return Response.json()
   }
-
-  res.status(200).json(response.data.choices[0].message.content)
 }
